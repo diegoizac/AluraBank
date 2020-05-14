d8c6017a09236b3c3a30aeeebe61fa2ef0c4e3d2
Diego Izac Luis da Silva <diegoizac@hotmail.com>
2020-05-14 20:25:02 -0300

    Inicio da parte dois do curso

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,8 +3,6 @@ class NegociacaoController {
   private _inputQuantidade: JQuery;
   private _inputValor: JQuery;
   private _negociacoes = new Negociacoes();
-  private _negociacoesView = new NegociacoesView("#negociacoesView");
-  private _mensagemView = new MensagemView("#mensagemView");
 
   constructor() {
     this._inputData = $("#data");
@@ -14,15 +12,14 @@ class NegociacaoController {
   }
 
   adiciona(event: Event) {
-
     event.preventDefault();
 
     const negociacao = new Negociacao(
       new Date(this._inputData.val().replace(/-/g, ",")),
       parseInt(this._inputQuantidade.val()),
       parseFloat(this._inputValor.val())
-      );
-      
+    );
+
     this._negociacoes.adiciona(negociacao);
 
     this._negociacoesView.update(this._negociacoes);
